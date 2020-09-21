77951ea6290661fd72de83cc0263b3b5318a7cb8
David Riedl <daves.weblab@gmail.com>
2020-09-21 21:38:20 +0200

    added component configs to hook

--- src/mobro/hooks/components-hooks.js
+++ src/mobro/hooks/components-hooks.js
@@ -18,13 +18,16 @@ export const withWrapper = createPublicHook("hooks.component", hooks => (compone
 });
 
 const components = {};
+const configs = {};
 
 /**
  * @param name
  * @param Component
+ * @param config
  */
-export function addComponent(name, Component) {
+export function addComponent(name, Component, config = {}) {
     addObjectPropertyByPath(components, name, Component);
+    addObjectPropertyByPath(configs, name, config);
 }
 
 registerPublicEndpoint("hooks.addComponent", addComponent);
@@ -38,3 +41,13 @@ export function getComponent(name) {
 }
 
 registerPublicEndpoint("hooks.getComponent", getComponent);
+
+/**
+ * @param name
+ * @returns {*}
+ */
+export function getComponentConfig(name) {
+    return configs[name];
+}
+
+registerPublicEndpoint("hooks.getComponentConfig", getComponentConfig);
