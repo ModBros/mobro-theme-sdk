3012e9e3993207b421aef9f55664c80425bf063c
David Riedl <david.riedl@elements.at>
2021-08-23 22:11:50 +0200

    fix(webpack): fixed output path.

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     },
 
     output: {
-        path: path.resolve(__dirname, "..", "mobro-desktop", "resources", "assets"),
+        path: path.resolve(__dirname, "dist"),
         filename: "[name]-v2.js"
     },
 
